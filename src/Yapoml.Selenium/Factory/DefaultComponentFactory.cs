13dc270ba1b77eafa1991e625a6dc76b028f5918
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2022-03-14 20:14:38 +0300

    Introduce IComponentFactory

---
+++ src/Yapoml.Selenium/Factory/DefaultComponentFactory.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using System;
+using Yapoml.Options;
+using Yapoml.Selenium.Components;
+
+namespace Yapoml.Selenium.Factory
+{
+    public class DefaultComponentFactory : IComponentFactory
+    {
+        public TComponent Create<TComponent>(IWebDriver webDriver, IWebElement webElement, ISpaceOptions spaceOptions) where TComponent : BaseComponent
+        {
+            var component = (TComponent)Activator.CreateInstance(typeof(TComponent), webDriver, webElement, spaceOptions);
+
+            return component;
+        }
+    }
+}
---
+++ src/Yapoml.Selenium/Factory/IComponentFactory.cs
@@ -0,0 +1,10 @@
+﻿using OpenQA.Selenium;
+using Yapoml.Selenium.Components;
+
+namespace Yapoml.Selenium.Factory
+{
+    public interface IComponentFactory
+    {
+        TComponent Create<TComponent>(IWebDriver webDriver, IWebElement webElement, Options.ISpaceOptions spaceOptions) where TComponent : BaseComponent;
+    }
+}
