bf04851552f6c1e22fdce3a5d154beb736afc721
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2022-03-14 19:57:17 +0300

    Introduce IPageFactory

---
+++ src/Yapoml.Selenium/Factory/DefaultPageFactory.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using System;
+using Yapoml.Options;
+using Yapoml.Selenium.Components;
+
+namespace Yapoml.Selenium.Factory
+{
+    public class DefaultPageFactory : IPageFactory
+    {
+        public TPage Create<TPage>(IWebDriver webDriver, ISpaceOptions spaceOptions) where TPage : BasePage
+        {
+            var page = (TPage)Activator.CreateInstance(typeof(TPage), webDriver, spaceOptions);
+
+            return page;
+        }
+    }
+}
---
+++ src/Yapoml.Selenium/Factory/IPageFactory.cs
@@ -0,0 +1,10 @@
+﻿using OpenQA.Selenium;
+using Yapoml.Selenium.Components;
+
+namespace Yapoml.Selenium.Factory
+{
+    public interface IPageFactory
+    {
+        TPage Create<TPage>(IWebDriver webDriver, Options.ISpaceOptions spaceOptions) where TPage : BasePage;
+    }
+}
