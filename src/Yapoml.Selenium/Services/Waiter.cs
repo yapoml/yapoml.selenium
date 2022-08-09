7e42909986b9d91f3d68d7abbfdc33786f6f5158
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2022-08-09 13:38:12 +0300

    Don't use humanizer for waiters

--- src/Yapoml.Selenium/Services/Waiter.cs
+++ src/Yapoml.Selenium/Services/Waiter.cs
@@ -1,10 +1,7 @@
-﻿using Humanizer;
-using OpenQA.Selenium;
+﻿using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
-using System.Reflection;
 using System.Text;
 using System.Threading;
 
@@ -12,11 +9,6 @@ namespace Yapoml.Selenium.Services
 {
     public static class Waiter
     {
-        static Waiter()
-        {
-            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
-        }
-
         public static TResult Until<TResult>(Func<TResult> condition, TimeSpan timeout, TimeSpan pollingInterval)
         {
             var stopwatch = Stopwatch.StartNew();
@@ -32,7 +24,7 @@ namespace Yapoml.Selenium.Services
                 Thread.Sleep(pollingInterval);
             }
 
-            throw new TimeoutException($"{condition} wasn't met during {timeout.Humanize()} and polling each {pollingInterval.Humanize()}.");
+            throw new TimeoutException($"{condition} wasn't met during {timeout.TotalSeconds} seconds and polling each {pollingInterval.TotalSeconds} seconds.");
         }
 
         public static IWebElement UntilDisplayed(string componentFriendlyName, ISearchContext searchContext, By by, TimeSpan timeout, TimeSpan pollingInterval)
@@ -80,7 +72,7 @@ namespace Yapoml.Selenium.Services
 
             builder.AppendLine();
 
-            builder.AppendLine($"  Timeout is {timeout.Humanize()} and polling each {pollingInterval.Humanize()}.");
+            builder.AppendLine($"  Timeout is {timeout.TotalSeconds} seconds and polling each {pollingInterval.TotalSeconds} seconds.");
 
             builder.AppendLine();
 
@@ -90,31 +82,11 @@ namespace Yapoml.Selenium.Services
 
                 foreach (var ignoredExceptionType in ignoredExceptionTypes)
                 {
-                    builder.AppendLine($"   - {ignoredExceptionType.Key.FullName} ({"time".ToQuantity(ignoredExceptionType.Value)})");
+                    builder.AppendLine($"   - {ignoredExceptionType.Key.FullName} ({ignoredExceptionType.Value} times)");
                 }
             }
 
             return new TimeoutException(builder.ToString(), innerException);
-        }
-
-        private static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
-        {
-            Assembly dep = null;
-
-            if (args.Name.StartsWith("Humanizer"))
-            {
-                var resourceName = "Yapoml.Selenium.Humanizer.dll";
-
-                using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
-                {
-                    using (BinaryReader reader = new BinaryReader(stream))
-                    {
-                        dep = Assembly.Load(reader.ReadBytes((int)stream.Length));
-                    }
-                }
-            }
-
-            return dep;
         }
     }
 }
