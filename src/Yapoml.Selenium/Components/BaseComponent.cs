d5cfd4193602214f097c96efd515814a12632fba
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2022-11-19 21:25:18 +0300

    Possibility to override default behavior for component actions

--- src/Yapoml.Selenium/Components/BaseComponent.cs
+++ src/Yapoml.Selenium/Components/BaseComponent.cs
@@ -1,10 +1,17 @@
 ﻿using OpenQA.Selenium;
+using System.Linq;
+using System.Reflection;
+using System;
 using Yapoml.Framework.Options;
 using Yapoml.Selenium.Events;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using Yapoml.Selenium.Options;
 
 namespace Yapoml.Selenium.Components
 {
-    public abstract class BaseComponent
+    /// <inheritdoc cref="IWebElement"/>
+    public abstract class BaseComponent : IWebElement, IWrapsElement, ITakesScreenshot
     {
         protected IWebDriver WebDriver { get; private set; }
 
@@ -21,6 +28,141 @@ namespace Yapoml.Selenium.Components
             SpaceOptions = spaceOptions;
 
             EventSource = spaceOptions.Services.Get<IEventSource>().ComponentEventSource;
+        }
+
+        public string TagName => WrappedElement.TagName;
+
+        public string Text => WrappedElement.Text;
+
+        public bool Enabled => WrappedElement.Enabled;
+
+        public bool Selected => WrappedElement.Selected;
+
+        public Point Location => WrappedElement.Location;
+
+        public Size Size => WrappedElement.Size;
+
+        public bool Displayed => WrappedElement.Displayed;
+
+        public void Clear()
+        {
+            WrappedElement.Clear();
+        }
+
+        public void Click()
+        {
+            WrappedElement.Click();
+        }
+
+        public IWebElement FindElement(By by)
+        {
+            return WrappedElement.FindElement(by);
+        }
+
+        public ReadOnlyCollection<IWebElement> FindElements(By by)
+        {
+            return WrappedElement.FindElements(by);
+        }
+
+        public string GetAttribute(string attributeName)
+        {
+            return WrappedElement.GetAttribute(attributeName);
+        }
+
+        public string GetCssValue(string propertyName)
+        {
+            return WrappedElement.GetCssValue(propertyName);
+        }
+
+        public string GetDomAttribute(string attributeName)
+        {
+            return WrappedElement.GetDomAttribute(attributeName);
+        }
+
+        public string GetDomProperty(string propertyName)
+        {
+            return WrappedElement.GetDomProperty(propertyName);
+        }
+
+        public ISearchContext GetShadowRoot()
+        {
+            return WrappedElement.GetShadowRoot();
+        }
+
+        public void SendKeys(string text)
+        {
+            WrappedElement.SendKeys(text);
+        }
+
+        public void Submit()
+        {
+            WrappedElement.Submit();
+        }
+
+        public Screenshot GetScreenshot()
+        {
+            return ((ITakesScreenshot)WrappedElement).GetScreenshot();
+        }
+
+        /// <summary>
+        /// Moves the cursor onto the element or one of its child elements.
+        /// </summary>
+        public virtual void Hover()
+        {
+            new OpenQA.Selenium.Interactions.Actions(WebDriver).MoveToElement(WrappedElement).Build().Perform();
+        }
+
+        /// <summary>
+        /// Moves the cursor onto the element or one of its child elements.
+        /// </summary>
+        public virtual void Hover(int x, int y)
+        {
+            new OpenQA.Selenium.Interactions.Actions(WebDriver).MoveToElement(WrappedElement, x, y).Build().Perform();
+        }
+
+        /// <summary>
+        /// Scrolls the element's ancestor containers is visible to the user.
+        /// </summary>
+        public virtual void ScrollIntoView()
+        {
+            if (SpaceOptions.Services.TryGet<ScrollIntoViewOptions>(out var options))
+            {
+                ScrollIntoView(options);
+            }
+            else
+            {
+                var js = "arguments[0].scrollIntoView();";
+
+                (WebDriver as IJavaScriptExecutor).ExecuteScript(js, WrappedElement);
+            }
+        }
+
+        /// <summary>
+        /// Scrolls the element's ancestor containers is visible to the user.
+        /// </summary>
+        public virtual void ScrollIntoView(ScrollIntoViewOptions options)
+        {
+            if (options == null) throw new ArgumentNullException(nameof(options));
+
+            var js = $"arguments[0].scrollIntoView({options});";
+
+            (WebDriver as IJavaScriptExecutor).ExecuteScript(js, WrappedElement);
+        }
+
+        /// <summary>
+        /// Determine whether component has private method with specific signature.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="methodName"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        protected bool HasImplementation<T>(string methodName, params object[] args) where T : BaseComponent
+        {
+            Type[] argTypes = args.Select(arg => arg.GetType()).ToArray();
+
+            var method = typeof(T).GetMethod(methodName, BindingFlags.NonPublic | BindingFlags.Instance, null, argTypes, null);
+
+            return method != null;
         }
     }
 }
