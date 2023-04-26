a7a93f384098f3ea93fbbb2ef6b5322665e0adf2
Nikolay Borisenko <22616990+nvborisenko@users.noreply.github.com>
2023-04-26 20:36:16 +0300

    Inline docs for WithBaseUrl extension

--- src/Yapoml.Selenium/Extensions/BaseUrlExtensions.cs
+++ src/Yapoml.Selenium/Extensions/BaseUrlExtensions.cs
@@ -1,21 +1,31 @@
-﻿using System;
-using Yapoml.Framework.Options;
+﻿using Yapoml.Framework.Options;
 using Yapoml.Selenium.Options;
 
 namespace Yapoml.Selenium
 {
+    /// <summary>
+    /// Various extensions to register base url.
+    /// </summary>
     public static class BaseUrlExtensions
     {
+        /// <summary>
+        /// Registers base url for pages navigation.
+        /// <example>
+        /// Usage:
+        /// <code>
+        ///     driver.Ya(opts => opts.WithBaseUrl("https://nuget.org"))
+        ///         .HomePage.Open();
+        /// </code>
+        /// </example>
+        /// </summary>
+        /// <param name="spaceOptions"></param>
+        /// <param name="url">The url to be registered as a base.</param>
+        /// <returns></returns>
         public static ISpaceOptions WithBaseUrl(this ISpaceOptions spaceOptions, string url)
         {
             spaceOptions.Services.Register(new BaseUrlOptions(url));
 
             return spaceOptions;
-        }
-
-        public static ISpaceOptions WithBaseUrl(this ISpaceOptions spaceOptions, Uri url)
-        {
-            return spaceOptions.WithBaseUrl(url.ToString());
         }
     }
 }
