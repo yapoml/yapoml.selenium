21029d8fea3c4ac6249769d164abacf27219d809
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2023-06-15 17:17:22 +0300

    Fix ci build don't use raw literals

--- src/Yapoml.Selenium/Components/BaseComponent.cs
+++ src/Yapoml.Selenium/Components/BaseComponent.cs
@@ -99,15 +99,15 @@ namespace Yapoml.Selenium.Components
         {
             get
             {
-                var js = """
-                    const rect = arguments[0].getBoundingClientRect();
-                    return (
-                        rect.top >= 0 &&
-                        rect.left >= 0 &&
-                        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-                        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-                    );
-                    """;
+                var js = @"
+const rect = arguments[0].getBoundingClientRect();
+return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+);";
+                    
 
                 return (bool)(RelocateOnStaleReference(() => (WebDriver as IJavaScriptExecutor).ExecuteScript(js, WrappedElement)));
             }
