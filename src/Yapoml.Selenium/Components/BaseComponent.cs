800c51341b954e5533ad4a7317b6a044757430d5
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2023-06-15 17:01:11 +0300

    IsInView property for component

--- src/Yapoml.Selenium/Components/BaseComponent.cs
+++ src/Yapoml.Selenium/Components/BaseComponent.cs
@@ -95,6 +95,24 @@ namespace Yapoml.Selenium.Components
 
         public bool IsSelected => RelocateOnStaleReference(() => WrappedElement.Selected);
 
+        public bool IsInView
+        {
+            get
+            {
+                var js = """
+                    const rect = arguments[0].getBoundingClientRect();
+                    return (
+                        rect.top >= 0 &&
+                        rect.left >= 0 &&
+                        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+                        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+                    );
+                    """;
+
+                return (bool)(RelocateOnStaleReference(() => (WebDriver as IJavaScriptExecutor).ExecuteScript(js, WrappedElement)));
+            }
+        }
+
         public Point Location => RelocateOnStaleReference(() => WrappedElement.Location);
 
         public Size Size => RelocateOnStaleReference(() => WrappedElement.Size);
