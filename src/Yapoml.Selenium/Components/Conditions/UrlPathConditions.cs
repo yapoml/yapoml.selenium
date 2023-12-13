155f30aeabb285c4ca66bf9b4a644d6458537c69
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2023-12-13 15:41:00 +0300

    Url path expectations

--- src/Yapoml.Selenium/Components/Conditions/UrlPathConditions.cs
+++ src/Yapoml.Selenium/Components/Conditions/UrlPathConditions.cs
@@ -8,94 +8,85 @@ using Yapoml.Selenium.Components.Metadata;
 namespace Yapoml.Selenium.Components.Conditions
 {
     /// <summary>
-    /// Various conditions for awaiting url.
+    /// Various conditions for awaiting url's path.
     /// </summary>
     /// <typeparam name="TConditions">Fluent original instance for chaining conditions.</typeparam>
-    public class UrlConditions<TConditions> : TextualConditions<TConditions>
+    public class UrlPathConditions<TConditions> : TextualConditions<TConditions>
     {
         private readonly IWebDriver _webDriver;
 
         private readonly PageMetadata _pageMetadata;
 
-        public UrlConditions(IWebDriver webDriver, TConditions conditions, TimeSpan timeout, TimeSpan pollingInterval, PageMetadata pageMetadata, ILogger logger)
-            : base(conditions, timeout, pollingInterval, $"{pageMetadata.Name} page url", logger)
+        public UrlPathConditions(IWebDriver webDriver, TConditions conditions, TimeSpan timeout, TimeSpan pollingInterval, PageMetadata pageMetadata, ILogger logger)
+            : base(conditions, timeout, pollingInterval, $"{pageMetadata.Name} page url path", logger)
         {
             _webDriver = webDriver;
             _pageMetadata = pageMetadata;
         }
 
-        protected override Func<string> FetchValueFunc => () => _webDriver.Url;
+        protected override Func<string> FetchValueFunc => () => new Uri(_webDriver.Url).AbsolutePath;
 
-        /// <summary>
-        /// Conditions for url's length.
-        /// </summary>
         public override NumericConditions<TConditions, int> Length
             => new TextualLengthConditons<TConditions>(_conditions, _timeout, _pollingInterval, FetchValueFunc, $"{_pageMetadata.Name} page url", _logger);
 
-        /// <summary>
-        /// Conditions for url's path.
-        /// </summary>
-        public UrlPathConditions<TConditions> Path
-            => new UrlPathConditions<TConditions>(_webDriver, _conditions, _timeout, _pollingInterval, _pageMetadata, _logger);
-
         protected override string GetIsError(string latestValue, string expectedValue)
         {
-            return $"{_pageMetadata.Name} page url is not '{expectedValue}',{GetDifference("it was:", expectedValue, latestValue)}";
+            return $"{_pageMetadata.Name} page url path is not '{expectedValue}',{GetDifference("it was:", expectedValue, latestValue)}";
         }
 
         protected override string GetIsNotError(string latestValue, string expectedValue)
         {
-            return $"{_pageMetadata.Name} page url is '{latestValue}', when expected to be not.";
+            return $"{_pageMetadata.Name} page url path is '{latestValue}', when expected to be not.";
         }
 
         protected override string GetIsEmptyError(string latestValue)
         {
-            return $"{_pageMetadata.Name} page url '{latestValue}' is not empty, when expected to be empty.";
+            return $"{_pageMetadata.Name} page url path '{latestValue}' is not empty, when expected to be empty.";
         }
 
         protected override string GetIsNotEmptyError(string latestValue)
         {
-            return $"{_pageMetadata.Name} page url is empty, when expected to be not empty.";
+            return $"{_pageMetadata.Name} page url path is empty, when expected to be not empty.";
         }
 
         protected override string GetStartsWithError(string latestValue, string expectedValue)
         {
-            return $"'{latestValue}' {_pageMetadata.Name} page url doesn't start with '{expectedValue}'.";
+            return $"'{latestValue}' {_pageMetadata.Name} page url path doesn't start with '{expectedValue}'.";
         }
 
         protected override string GetDoesNotStartWithError(string latestValue, string expectedValue)
         {
-            return $"'{latestValue}' {_pageMetadata.Name} page url starts with '{expectedValue}'.";
+            return $"'{latestValue}' {_pageMetadata.Name} page url path starts with '{expectedValue}'.";
         }
 
         protected override string GetEndsWithError(string latestValue, string expectedValue)
         {
-            return $"'{latestValue}' {_pageMetadata.Name} page url doesn't end with '{expectedValue}'.";
+            return $"'{latestValue}' {_pageMetadata.Name} page url path doesn't end with '{expectedValue}'.";
         }
 
         protected override string GetDoesNotEndWithError(string latestValue, string expectedValue)
         {
-            return $"'{latestValue}' {_pageMetadata.Name} page url ends with '{expectedValue}'.";
+            return $"'{latestValue}' {_pageMetadata.Name} page url path ends with '{expectedValue}'.";
         }
 
         protected override string GetContainsError(string latestValue, string expectedValue)
         {
-            return $"'{latestValue}' {_pageMetadata.Name} page url doesn't contain '{expectedValue}' yet.";
+            return $"'{latestValue}' {_pageMetadata.Name} page url path doesn't contain '{expectedValue}' yet.";
         }
 
         protected override string GetDoesNotContainError(string latestValue, string expectedValue)
         {
-            return $"'{latestValue}' {_pageMetadata.Name} page url contains '{expectedValue}'.";
+            return $"'{latestValue}' {_pageMetadata.Name} page url path contains '{expectedValue}'.";
         }
 
         protected override string GetMatchesError(string latestValue, Regex regex)
         {
-            return $"'{latestValue}' {_pageMetadata.Name} page url doesn't match '{regex}'.";
+            return $"'{latestValue}' {_pageMetadata.Name} page url path doesn't match '{regex}'.";
         }
 
         protected override string GetDoesNotMatchError(string latestValue, Regex regex)
         {
-            return $"'{latestValue}' {_pageMetadata.Name} page url matches '{regex}'.";
+            return $"'{latestValue}' {_pageMetadata.Name} page url path matches '{regex}'.";
         }
     }
 }
